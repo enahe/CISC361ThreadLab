4c0d68ddbf80004067b90fb6b504473917451153
Eric Nahe <enahe@udel.edu>
2018-10-29 20:06:31 -0700

    Added create function, now for yield

--- t_lib.c
+++ t_lib.c
@@ -21,23 +21,26 @@ void t_init()
   initialSetup  = (tcb *) malloc(sizeof(tcb));
   ucontext_t *tmp;
   tmp = (ucontext_t *) malloc(sizeof(ucontext_t));
-  initialSetup -> thread_id = 1;
-  initialSetup -> thread_priority = 1;
+ 
+  getcontext(tmp);    /* let tmp be the context of main() */
+  initialSetup -> thread_id = 0;
+  initialSetup -> thread_priority = 0;
   initialSetup -> thread_context = *tmp;
   initialSetup -> next = NULL;
-  getcontext(&(initialSetup->thread_context));    /* let tmp be the context of main() */
   printf("Starting new context at %p\n", (void *) &(initialSetup->thread_context));
   running = initialSetup;
   printf("Starting new running thread at %p, with thread id %d \n", (void *) running);
 }
 
-void t_create(void(*function)(int), int thread_id, int priority)
+void t_create(void(*function)(int), int id, int priority)
 {
+  printf("Here");
   size_t sz = 0x10000;
-
+  tcb *addTcb;
+  tcb *tempTcb = ready;
+  addTcb = (tcb *) malloc(sizeof(tcb));
   ucontext_t *uc;
   uc = (ucontext_t *) malloc(sizeof(ucontext_t));
-
   getcontext(uc);
 /***
   uc->uc_stack.ss_sp = mmap(0, sz,
@@ -47,7 +50,23 @@ void t_create(void(*function)(int), int thread_id, int priority)
   uc->uc_stack.ss_sp = malloc(sz);  /* new statement */
   uc->uc_stack.ss_size = sz;
   uc->uc_stack.ss_flags = 0;
-  uc->uc_link = running; 
-  makecontext(uc, (void (*)(void)) function, 1, thread_id);
-  ready = uc;
+  uc->uc_link = &(running->thread_context); 
+  makecontext(uc, (void (*)(void)) function, 1, id);
+  addTcb -> thread_id = id;
+  addTcb -> thread_priority = priority;
+  addTcb -> thread_context = *uc;
+  addTcb -> next = NULL;
+  printf("Starting new context at %p\n", (void *) &(addTcb->thread_context));
+  if (ready == NULL) {
+     ready = addTcb;
+     printf("Starting new ready thread at %p, with thread id %d \n", (void *) ready);
+  }
+  else {
+     while (tempTcb -> next != NULL) {
+     tempTcb = tempTcb -> next;
+     }
+     tempTcb-> next = addTcb;
+     printf("adding to the end of the tcb");
+  }
+
 }
--- test01.c
+++ test01.c
@@ -11,11 +11,11 @@ void function(int thr_id)
 
    for (i = j = 0; i < 3; i++, j++) {
       printf("this is thread %d [%d]...\n", thr_id, j);
-      //t_yield();
+      t_yield();
    }
 
    printf("Thread %d is done...\n", thr_id);
-   //t_terminate();
+   t_terminate();
 }
 
 int main(void)
@@ -28,17 +28,16 @@ int main(void)
    t_create(function, 2, 1);
    printf("This is main(2)...\n");
    t_create(function, 3, 1);
-/*
+
    for (i = 0; i < 4; i++) {
       printf("This is main(3)[%d]...\n", i);
-    //  t_yield();
+      t_yield();
    }
 
    printf("Begin shutdown...\n");
-  // t_shutdown();
+   t_shutdown();
    printf("Done with shutdown...\n");
 
    return 0;
-*/
 }
 
